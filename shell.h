0df1d2085af276115f45df6aea44b30e9cabfc56
Ami Manye <amimanyeaglago@gmail.com>
2023-09-18 14:13:46 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* global variables */
+extern char **environ;
+#endif
